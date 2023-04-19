4035fc9bc93e4f47b0b6671e33d8b4d7b771e5cb
thomas.hu <thomas.hu@bayhubtech.com>
2023-04-19 16:41:06 +0800

    refactor setting

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         pygame.init() #初始化pygame实例
-        self.screen = pygame.display.set_mode((1200, 800)) #创建窗口，本质是构建surface对象，并赋值给类的screen
-        pygame.display.set_caption("Alien Invasion") #设置标题
-        self.bg_color = (230,230,230) #以RGB指定背景颜色
+        
+        #初始化AlienInvasion的成员
+        self.settings = Settings() #构造Settings类型的成员
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        
+        pygame.display.set_caption("Alien Invasion")
         
     def run_game(self): #枚举的event检测去对surface对象刷新重绘或退出
         while True: #持续监听事件并处理
@@ -14,7 +18,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
-            self.screen.fill(self.bg_color) #填充颜色到surface类型的screen对象
+            self.screen.fill(self.settings.bg_color) #填充颜色到surface类型的screen对象
             pygame.display.flip() #刷新窗口：重绘surface并覆盖旧的surface
 
 if __name__ == '__main__':
---
+++ alien-invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """所有设置用此类管理"""
+    
+    def __init__(self):
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
