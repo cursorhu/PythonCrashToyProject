7b0be150336e0d3533745e0378ea2bff2282e247
thomas.hu <thomas.hu@bayhubtech.com>
2023-04-19 16:26:46 +0800

    add bg_color

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -6,6 +6,7 @@ class AlienInvasion:
         pygame.init() #初始化pygame实例
         self.screen = pygame.display.set_mode((1200, 800)) #创建窗口，本质是构建surface对象，并赋值给类的screen
         pygame.display.set_caption("Alien Invasion") #设置标题
+        self.bg_color = (230,230,230) #以RGB指定背景颜色
         
     def run_game(self): #枚举的event检测去对surface对象刷新重绘或退出
         while True: #持续监听事件并处理
@@ -13,6 +14,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            self.screen.fill(self.bg_color) #填充颜色到surface类型的screen对象
             pygame.display.flip() #刷新窗口：重绘surface并覆盖旧的surface
 
 if __name__ == '__main__':
